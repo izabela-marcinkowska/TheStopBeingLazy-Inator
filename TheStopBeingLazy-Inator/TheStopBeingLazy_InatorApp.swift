09686ad5e81e77c330df39333a02a700c10344dd
Devy <izabela.marcinkowska@outlook.com>
2024-11-28 13:29:04 +0100

    first commit

---
+++ TheStopBeingLazy-Inator/TheStopBeingLazy_InatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheStopBeingLazy_InatorApp.swift
+//  TheStopBeingLazy-Inator
+//
+//  Created by Izabela Marcinkowska on 2024-11-28.
+//
+
+import SwiftUI
+
+@main
+struct TheStopBeingLazy_InatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
